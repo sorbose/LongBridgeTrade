75fdebd5a48af5891f024379f81a278b87bf56c2
mingzi <59370234+sorbose@users.noreply.github.com>
2024-11-16 23:58:31 +0800

    feat 获取单只股票所有1分钟K线

--- src/main/java/io/github/sorbose/lbtrade/quote/CandlesticksFetcher.java
+++ src/main/java/io/github/sorbose/lbtrade/quote/CandlesticksFetcher.java
@@ -1,14 +1,17 @@
 package io.github.sorbose.lbtrade.quote;
 
 
-import com.longport.OpenApiException;
+import com.longport.Config;
 import com.longport.quote.AdjustType;
 import com.longport.quote.Candlestick;
 import com.longport.quote.Period;
 import com.longport.quote.QuoteContext;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -20,32 +23,56 @@ import org.apache.logging.log4j.Logger;
 public class CandlesticksFetcher {
     private static final Logger logger = LogManager.getLogger(CandlesticksFetcher.class);
 
-    private Candlestick[] fetch(String symbol, LocalDate start, LocalDate end) {
-        try (QuoteContext quoteContext = new QuoteContext()) {
-            CompletableFuture<Candlestick[]> future = quoteContext.getHistoryCandlesticksByDate
-                    (symbol, Period.Min_1, AdjustType.ForwardAdjust, start, end);
-            return future.get(10, TimeUnit.SECONDS);
+    private List<Candlestick> fetch1Min(String symbol, LocalDateTime start) {
+        LocalDateTime current = start;
+        List<Candlestick> list = new ArrayList<>();
+        try (Config config = Config.fromEnv(); QuoteContext quoteContext =QuoteContext.create(config).get()) {
+            int count=1000;
+            while(count>0){
+                CompletableFuture<Candlestick[]> future = quoteContext.getHistoryCandlesticksByOffset(
+                        symbol, Period.Min_1, AdjustType.ForwardAdjust, true, current, 1000
+                );
+                Candlestick[] result = future.get(100, TimeUnit.SECONDS);
+                count=result==null?0:result.length;
+                if (result != null) {
+                    if(result[result.length-1].getTimestamp().toLocalDateTime().isBefore(current)){
+                        break;
+                    }
+                    list.addAll(Arrays.asList(result));
+                }
+                current=list.get(list.size()-1).getTimestamp().toLocalDateTime().plusMinutes(1);
+                System.out.println(current);
+                System.out.println(count);
+            }
         } catch (TimeoutException e) {
             logger.warn("Fetching data timed out for symbol: {}", symbol, e);
-            return new Candlestick[0]; // 返回空数据
         } catch (Exception e) {
             logger.error("Error fetching data for symbol: {}", symbol, e);
             throw new RuntimeException("Error fetching data for symbol: " + symbol, e);
         }
+        return list;
     }
 
-    public void toConsole(String symbol, LocalDate start, LocalDate end) {
-        Arrays.stream(fetch(symbol, start, end)).forEach(System.out::println);
+    public void toConsole(String symbol, LocalDateTime start) {
+        fetch1Min(symbol, start).forEach(System.out::println);
     }
 
-    public void toCsv(String symbol, LocalDate start, LocalDate end) {
-        CSVSerializer.writeObjectsToCsv(Arrays.asList(fetch(symbol, start, end)),symbol+start+end);
+    public void toCsv(String symbol, LocalDateTime start) {
+        CSVSerializer.writeObjectsToCsv(fetch1Min(symbol, start),
+                ("data/"+symbol+start).replaceAll(":", ""));
     }
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getenv("LONGPORT_APP_KEY"));
+        logger.debug(System.getProperty("os.arch"));
+        logger.info("start");
         CandlesticksFetcher fetcher = new CandlesticksFetcher();
-        fetcher.toConsole("TSLA.US", LocalDate.of(2024, 11, 10), LocalDate.of(2024, 11, 17));
+        long startTime = System.currentTimeMillis();
+        fetcher.toCsv("TSLQ.US",
+                LocalDate.of(2023, 12, 4).atStartOfDay());
+        long endTime = System.currentTimeMillis();
+        logger.info("end");
+        logger.info("Time taken (s): " + (endTime - startTime)/1000f);
     }
 
 }
